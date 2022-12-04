489bc3c4d7b6b8459945e4809f8e77c6a73223f0
rudenkooleksei@gmail.com <rudenkooleksei@gmail.com>
2022-12-04 16:55:41 +0200

    first commit

--- main.py
+++ main.py
@@ -1,51 +1,86 @@
+import os
 from pytube import YouTube
-import telebot
-from telebot import types # для указание типов
-from telebot.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram import *
 from config import token
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-bot = telebot.TeleBot(token=token)
-
-
-# markup = ReplyKeyboardMarkup(resize_keyboard=True)
-#
-# btn1 = types.KeyboardButton("Показать")
-# btn2 = types.KeyboardButton("Посчитать")
-# btn3 = types.KeyboardButton("Ввести стоимость за литр")
-# markup.add(btn1, btn2)
-# markup.add(btn3)
-#
-keyboard = InlineKeyboardMarkup()
-keyboard.add(InlineKeyboardButton(text="Умножить на грн", callback_data="value"))
-
-#youtube_video_url = 'https://www.youtube.com/watch?v=DkU9WFj8sYo'
-
-#yt_obj = YouTube(youtube_video_url)
+bot = Bot(token)
+dp = Dispatcher(bot)
 
 
-@bot.message_handler(content_types=['text'])
-def buttons_menu(message):
-    print(message)
-    print(message.text)
-    if message.text[:7] == "https:/":
-        try:
-            yt_obj = YouTube(message.text)
-            song_title = yt_obj.title
-            file_name = f"{song_title}" + ".mp3"
+@dp.message_handler()
+async def text_message(message:types.Message):
+    chat_id = message.chat.id
+    url = message.text
+    print(len(url.encode()))
+    yt = YouTube(url)
 
-            yt_obj.streams.get_audio_only().download(filename=file_name)
-            print('YouTube video audio downloaded successfully')
-            audio = open(file_name, 'rb')
-            print("audio was open")
-            bot.send_audio(message.chat.id, audio)
-            print("audio was send")
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(InlineKeyboardButton(text="720p", callback_data=f"video|{url}|{chat_id}"))
+    keyboard.add(InlineKeyboardButton(text="Music", callback_data=f"music|{url}|{chat_id}"))
 
-        except Exception as e:
-            print(e)
-
-    else:
-        print("unvalid link")
-        bot.send_message(message.chat.id, "Эта ссылка не работает")
+    if message.text.startswith("https://www.youtube.com"):
+        await bot.send_message(chat_id, f"{yt.title}",
+                               reply_markup=keyboard,
+                               parse_mode="Markdown")
 
 
-bot.polling()
+@dp.message_handler(commands=["start"])
+async def start_message(message:types.Message):
+    chat_id = message.chat.id
+    await bot.send_message(chat_id, "Привет, чтобы скачать видео отправь мне ссылку на видео с YouTube!")
+
+
+async def download_youtube_video(url, chat_id, bot):
+    try:
+        yt = YouTube(url)
+        await bot.send_message(chat_id, "Файл загружается..")
+
+        stream = yt.streams.filter(progressive=True, file_extension="mp4")
+        stream.get_highest_resolution().download(f"{chat_id}", f"{chat_id}_{yt.title}")
+
+        with open(f"{chat_id}/{chat_id}_{yt.title}", "rb") as video:
+            await bot.send_video(chat_id, video, parse_mode="Markdown")
+            os.remove(f"{chat_id}/{chat_id}_{yt.title}")
+    except:
+        await bot.send_message(chat_id, "Не удалось загрузить видеофайл")
+
+
+async def download_youtube_music(url, chat_id, bot):
+    try:
+        yt = YouTube(url)
+        await bot.send_message(chat_id, "Файл загружается..")
+
+        audio = yt.streams.get_audio_only()
+        audio.download(f"{chat_id}", f"{yt.title}.mp3")
+
+        with open(f"{chat_id}/{yt.title}.mp3", "rb") as music:
+            print("1")
+            await bot.send_audio(chat_id, music, parse_mode="Markdown")
+            print("2")
+            #os.remove(f"{chat_id}/{chat_id}_{yt.title}")
+    except:
+        await bot.send_message(chat_id, "Не удалось загрузить аудиофайл")
+
+
+# @dp.callback_query_handler(lambda c: c.data == 'video')
+# async def send_video(callback_query: types.CallbackQuery):
+#     await callback_query.message.answer('Видео вроде как должно было начать загружаться, но..')
+
+
+@dp.callback_query_handler(lambda c: c.data.startswith('video'))
+async def send_video(callback_query: types.CallbackQuery):
+    _, url, chat_id = callback_query.data.split("|")
+    print(url, chat_id)
+    await download_youtube_video(url, chat_id, bot)
+    print("It works!")
+
+
+@dp.callback_query_handler(lambda c: c.data.startswith('music'))
+async def send_audio(callback_query: types.CallbackQuery):
+     _, url, chat_id = callback_query.data.split("|")
+     await download_youtube_music(url, chat_id, bot)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
