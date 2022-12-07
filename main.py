cde3497560d5757b635e24243780b82e1a91726f
rudenkooleksei@gmail.com <rudenkooleksei@gmail.com>
2022-12-07 14:42:06 +0200

    Now bot can receive and work with link more than 64 symbols

--- main.py
+++ main.py
@@ -7,22 +7,28 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 bot = Bot(token)
 dp = Dispatcher(bot)
 
+arguments = []
+
 
 @dp.message_handler()
 async def text_message(message:types.Message):
     chat_id = message.chat.id
     url = message.text
-    print(len(url.encode()))
     yt = YouTube(url)
 
+    arguments.append(url)
+    arguments.append(chat_id)
+
     keyboard = InlineKeyboardMarkup()
-    keyboard.add(InlineKeyboardButton(text="720p", callback_data=f"video|{url}|{chat_id}"))
-    keyboard.add(InlineKeyboardButton(text="Music", callback_data=f"music|{url}|{chat_id}"))
+    keyboard.add(InlineKeyboardButton(text="720p", callback_data=f"video|{0}|{1}"))
+    keyboard.add(InlineKeyboardButton(text="Music", callback_data=f"music|{0}|{1}"))
 
     if message.text.startswith("https://www.youtube.com"):
         await bot.send_message(chat_id, f"{yt.title}",
                                reply_markup=keyboard,
                                parse_mode="Markdown")
+
+
 
 
 @dp.message_handler(commands=["start"])
@@ -53,11 +59,11 @@ async def download_youtube_music(url, chat_id, bot):
 
         audio = yt.streams.get_audio_only()
         audio.download(f"{chat_id}", f"{yt.title}.mp3")
+        print("file was open")
+
 
         with open(f"{chat_id}/{yt.title}.mp3", "rb") as music:
-            print("1")
             await bot.send_audio(chat_id, music, parse_mode="Markdown")
-            print("2")
             #os.remove(f"{chat_id}/{chat_id}_{yt.title}")
     except:
         await bot.send_message(chat_id, "Не удалось загрузить аудиофайл")
@@ -70,16 +76,14 @@ async def download_youtube_music(url, chat_id, bot):
 
 @dp.callback_query_handler(lambda c: c.data.startswith('video'))
 async def send_video(callback_query: types.CallbackQuery):
-    _, url, chat_id = callback_query.data.split("|")
-    print(url, chat_id)
-    await download_youtube_video(url, chat_id, bot)
-    print("It works!")
+    _, url_index, chat_id_index = callback_query.data.split("|")
+    await download_youtube_video(arguments[int(url_index)], arguments[int(chat_id_index)], bot)
 
 
 @dp.callback_query_handler(lambda c: c.data.startswith('music'))
 async def send_audio(callback_query: types.CallbackQuery):
-     _, url, chat_id = callback_query.data.split("|")
-     await download_youtube_music(url, chat_id, bot)
+    _, url_index, chat_id_index = callback_query.data.split("|")
+    await download_youtube_music(arguments[int(url_index)], arguments[int(chat_id_index)], bot)
 
 
 if __name__ == "__main__":
